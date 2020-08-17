7301328fbc9589fca278e3b524d6437e3e90cebe
paul handcock <phandcock@live.com.au>
2020-08-17 21:52:29 +0800

    test: tweak test project

---
+++ GrampsView.UnitTest.UWP/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrampsView.UnitTest.UWP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GrampsView.UnitTest.UWP/ViewModels/MinorPages/MainPageViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿namespace GrampsViewUnitTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class MainPageViewModelTest
+    {
+
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+        
+
+
+        }
+
+        [TestMethod]
+        public void NavigateShouldCallNavigate()
+        {
+        }
+    }
+}
