1463e401718b2dc45ab7df29497db34308160d4d
paul handcock <phandcock@live.com.au>
2020-08-30 16:15:40 +0800

    feat: start work on adding shell

---
+++ GrampsView/ViewModels/MinorPages/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GrampsView.ViewModels.MinorPages
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
