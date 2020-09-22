6df5791915064d2bca312167dca6772b16ec0c04
paul handcock <phandcock@live.com.au>
2020-09-22 20:43:46 +0800

    test: change to nunit

---
+++ GrampsView.NUnit.Test/Models/DataModels/testPersonModel.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="testPersonModel.cs" company="PlaceholderCompany">
+//     Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// </summary>
+namespace GrampsViewXUnit.Data.Models.DataModels
+{
+    using GrampsView.Data.Model;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using System;
+
+    /// <summary>
+    /// </summary>
+    public class TestPersonModel
+    {
+        /// <summary>
+        /// Tests the basic person details.
+        /// </summary>
+        //[Fact]
+        public void TestBasicPersonDetails()
+        {
+            // Setup BirthDate
+            string aCFormat = "a";
+            bool aDualDated = false;
+            string aNewYear = "a";
+            string aQuality = "a";
+            string aStart = "a";
+            string aStop = "a";
+            string aVal = DateTime.Now.AddYears(-17).ToUniversalTime().ToString("r"); // To Universal time like Gramps uses.
+            string aValType = "a";
+
+            //DateObjectModelVal birthdatemodel = new DateObjectModelVal(aCFormat, aDualDated, aNewYear, aQuality, aStart, aStop, aVal, aValType)
+            //{
+            //};
+
+            PersonModel testPerson = new PersonModel
+            {
+                //BirthDate = birthdatemodel,
+            };
+
+            // Test assigned correctly
+            Assert.AreNotEqual(testPerson.BirthDate, aCFormat);
+            Assert.AreNotEqual(testPerson.BirthDate, aDualDated);
+            Assert.AreNotEqual(testPerson.BirthDate, aNewYear);
+            Assert.AreNotEqual(testPerson.BirthDate, aQuality);
+            Assert.AreNotEqual(testPerson.BirthDate, aStart);
+            Assert.AreNotEqual(testPerson.BirthDate, aStop);
+            Assert.AreNotEqual(testPerson.BirthDate, aVal);
+            Assert.AreNotEqual(testPerson.BirthDate, aValType);
+
+            // Test Age is correct
+            //Assert.True(testPerson.BirthDate.GetAge == 17);
+        }
+    }
+}
---
+++ GrampsView.NUnit.Test/ViewModels/Sources/SourceDetailViewModelTest.cs
@@ -0,0 +1,63 @@
+﻿namespace GrampsViewUnitTest
+{
+    using GrampsView.Common;
+    using GrampsView.ViewModels;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Prism.Events;
+    using Prism.Navigation;
+
+    [TestClass]
+    public class SourceDetailViewModelTest
+    {
+        private Mock<ICommonLogging> MockIocCommonLogging;
+
+        private Mock<IEventAggregator> MockIocEventAggregator;
+
+        private Mock<INavigationService> MockIocNavigationService;
+
+        private SourceDetailViewModel MockSourceDetailViewModel;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            MockIocCommonLogging = null;
+
+            MockIocEventAggregator = null;
+
+            MockIocNavigationService = null;
+
+            MockSourceDetailViewModel = null;
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            MockIocCommonLogging = new Mock<ICommonLogging>();
+
+            MockIocEventAggregator = new Mock<IEventAggregator>();
+
+            MockIocNavigationService = new Mock<INavigationService>();
+
+            MockSourceDetailViewModel = new SourceDetailViewModel(MockIocCommonLogging.Object, MockIocEventAggregator.Object, MockIocNavigationService.Object);
+        }
+
+        [TestMethod]
+        public void PopulateModelViewTest()
+        {
+            // arrrange
+
+            // act
+            //MockSourceDetailViewModel.PopulateViewModel();
+
+            // assert
+            //HLinkMediaModel SourceMediaModel = MockSourceDetailViewModel.BaseDetail[0] as HLinkMediaModel;
+            //Assert.IsFalse(SourceMediaModel == null);
+
+            //Assert.IsTrue(SourceMediaModel.CardType == DisplayFormat.MediaCardLarge);
+        }
+    }
+}
