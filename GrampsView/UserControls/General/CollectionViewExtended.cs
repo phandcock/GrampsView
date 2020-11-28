8b36a1aa77a8b3a03489c97cd4f698fd1ed8383e
paul handcock <phandcock@live.com.au>
2020-11-28 17:18:12 +0800

    fix: tweak collectionView Rows

---
+++ GrampsView/UserControls/General/CollectionViewExtended.cs
@@ -0,0 +1,43 @@
+﻿namespace GrampsView.UserControls
+{
+    using Xamarin.Forms;
+
+    public class CollectionViewExtended : CollectionView
+    {
+        private int _NumColumns = 10;
+
+        public CollectionViewExtended()
+        {
+            this.ItemSizingStrategy = ItemSizingStrategy.MeasureAllItems;
+
+            //GridItemsLayout newGridLayout = new GridItemsLayout(ItemsLayoutOrientation.Vertical)
+            //{
+            //    HorizontalItemSpacing = 2,
+            //    VerticalItemSpacing = 2,
+            //    Span = NumColumns
+            //};
+
+            //this.ItemsLayout = newGridLayout;
+        }
+
+        /// <summary>
+        /// Gets or sets the number columns to display.
+        /// </summary>
+        /// <value>
+        /// The number columns.
+        /// </value>
+        //public int NumColumns
+        //{
+        //    get
+        //    {
+        //        return _NumColumns;
+        //    }
+
+        //    set
+        //    {
+        //        _NumColumns = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+    }
+}
