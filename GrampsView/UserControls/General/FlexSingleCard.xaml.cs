7edca0a4f0a375ae58ec79c92aba3476e7684586
paul handcock <phandcock@live.com.au>
2020-11-30 02:59:55 +0800

    fix: tweal flexcardsingle control

--- GrampsView/UserControls/General/FlexSingleCard.xaml.cs
+++ GrampsView/UserControls/General/FlexSingleCard.xaml.cs
@@ -10,13 +10,13 @@
     public partial class FlexSingleCard : Frame, INotifyPropertyChanged
     {
         public static readonly BindableProperty FsctSourceProperty
-              = BindableProperty.Create(returnType: typeof(IEnumerable), declaringType: typeof(CollectionSingleCard), propertyName: nameof(FsctSource), propertyChanged: OnItemsSourceChanged);
+              = BindableProperty.Create(returnType: typeof(IEnumerable), declaringType: typeof(CollectionSingleCard), propertyName: nameof(FsctSource)); //, propertyChanged: OnItemsSourceChanged);
 
         public static readonly BindableProperty FsctTemplateProperty
-                    = BindableProperty.Create(nameof(FsctTemplate), returnType: typeof(DataTemplate), declaringType: typeof(CollectionSingleCard), propertyChanged: OnItemTemplateChanged);
+                    = BindableProperty.Create(nameof(FsctTemplate), returnType: typeof(DataTemplate), declaringType: typeof(CollectionSingleCard)); //, propertyChanged: OnItemTemplateChanged);
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CollectionSingleCard"/> class.
+        /// Initializes a new instance of the <see cref="FlexSingleCard"/> class.
         /// </summary>
         public FlexSingleCard()
         {
@@ -47,77 +47,77 @@
             set { SetValue(FsctTemplateProperty, value); }
         }
 
-        public static void OnItemsSourceChanged(BindableObject argSource, object oldValue, object newValue)
-        {
-            Contract.Assert(argSource != null);
+        //public static void OnItemsSourceChanged(BindableObject argSource, object oldValue, object newValue)
+        //{
+        //    Contract.Assert(argSource != null);
 
-            if (newValue is null)
-            {
-                return;
-            }
+        //    if (newValue is null)
+        //    {
+        //        return;
+        //    }
 
-            CollectionSingleCard layout = argSource as CollectionSingleCard;
-            Contract.Requires(layout != null);
+        //    CollectionSingleCard layout = argSource as CollectionSingleCard;
+        //    Contract.Requires(layout != null);
 
-            IEnumerable iSource = newValue as IEnumerable;
+        //    IEnumerable iSource = newValue as IEnumerable;
 
-            // layout.theCollectionView.ItemsSource = iSource;
+        //    // layout.theCollectionView.ItemsSource = iSource;
 
-            int Counter = 0;
+        //    int Counter = 0;
 
-            foreach (var item in iSource)
-            {
-                Counter++;
-            }
+        //    foreach (var item in iSource)
+        //    {
+        //        Counter++;
+        //    }
 
-            // layout.theCollectionView.NumRows = Counter;
+        //    // layout.theCollectionView.NumRows = Counter;
 
-            // layout.theCollectionView.SetUcHeight();
-        }
+        //    // layout.theCollectionView.SetUcHeight();
+        //}
 
-        /// <summary>
-        /// Called when [item template changed].
-        /// </summary>
-        /// <param name="argSource">
-        /// The argument source.
-        /// </param>
-        /// <param name="oldValue">
-        /// The old value.
-        /// </param>
-        /// <param name="newValue">
-        /// The new value.
-        /// </param>
-        public static void OnItemTemplateChanged(BindableObject argSource, object oldValue, object newValue)
-        {
-            Contract.Requires(argSource != null);
-            Contract.Requires(newValue != null);
+        ///// <summary>
+        ///// Called when [item template changed].
+        ///// </summary>
+        ///// <param name="argSource">
+        ///// The argument source.
+        ///// </param>
+        ///// <param name="oldValue">
+        ///// The old value.
+        ///// </param>
+        ///// <param name="newValue">
+        ///// The new value.
+        ///// </param>
+        //public static void OnItemTemplateChanged(BindableObject argSource, object oldValue, object newValue)
+        //{
+        //    Contract.Requires(argSource != null);
+        //    Contract.Requires(newValue != null);
 
-            CollectionSingleCard layout = argSource as CollectionSingleCard;
-            Contract.Requires(layout != null);
+        //    CollectionSingleCard layout = argSource as CollectionSingleCard;
+        //    Contract.Requires(layout != null);
 
-            DataTemplate iTemplate = newValue as DataTemplate;
+        //    DataTemplate iTemplate = newValue as DataTemplate;
 
-            // layout.theCollectionView.ItemTemplate = iTemplate;
-        }
+        //    // layout.theCollectionView.ItemTemplate = iTemplate;
+        //}
 
-        /// <summary>
-        /// Handles the SizeChanged event of the CollectionSingleCardRoot control.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event.
-        /// </param>
-        /// <param name="e">
-        /// The <see cref="EventArgs"/> instance containing the event data.
-        /// </param>
-        private void CollectionSingleCardRoot_SizeChanged(object sender, EventArgs e)
-        {
-            Contract.Requires(sender != null);
+        ///// <summary>
+        ///// Handles the SizeChanged event of the CollectionSingleCardRoot control.
+        ///// </summary>
+        ///// <param name="sender">
+        ///// The source of the event.
+        ///// </param>
+        ///// <param name="e">
+        ///// The <see cref="EventArgs"/> instance containing the event data.
+        ///// </param>
+        //private void CollectionSingleCardRoot_SizeChanged(object sender, EventArgs e)
+        //{
+        //    Contract.Requires(sender != null);
 
-            CollectionSingleCard t = sender as CollectionSingleCard;
+        //    CollectionSingleCard t = sender as CollectionSingleCard;
 
-            //t.theCollectionView.NumColumns = (Int32)(t.Width / CardSizes.Current.CardSmallWidth);  // +1 for padding
+        //    //t.theCollectionView.NumColumns = (Int32)(t.Width / CardSizes.Current.CardSmallWidth);  // +1 for padding
 
-            //t.theCollectionView.setUcHeight();
-        }
+        //    //t.theCollectionView.setUcHeight();
+        //}
     }
 }
