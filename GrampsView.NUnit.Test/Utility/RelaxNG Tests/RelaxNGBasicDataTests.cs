18c1b140e1b2a154bed5a06fb8d43265485810de
paul handcock <phandcock@live.com.au>
2021-01-26 18:30:02 +0800

    test: start work on gpkg data load tests

---
+++ GrampsView.NUnit.Test/Utility/RelaxNG Tests/RelaxNGBasicDataTests.cs
@@ -0,0 +1,33 @@
+﻿namespace GrampsView.Data.Model.Tests
+{
+    using Commons.Xml.Relaxng;
+
+    using global::NUnit.Framework;
+
+    using System.Xml;
+
+    [TestFixture()]
+    public class RelaxNGBasicDataTests
+    {
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [Test()]
+        public void RelaxNGBasicDataTests_Basic()
+        {
+            XmlReader instance = new XmlTextReader("instance.xml");
+
+            XmlReader grammar = new XmlTextReader("my.rng");
+
+            RelaxngValidatingReader reader =
+                new RelaxngValidatingReader(instance, grammar);
+        }
+    }
+}
