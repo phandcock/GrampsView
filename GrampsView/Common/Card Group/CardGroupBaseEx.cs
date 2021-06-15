9c2af5f23c49ecb5ce77138f6f6e97fb9d5f4094
paul handcock <phandcock@live.com.au>
2021-06-16 03:27:34 +0800

    fix: work on breadcrumbs and titleview

---
+++ GrampsView/Common/Card Group/CardGroupBaseEx.cs
@@ -0,0 +1,113 @@
+﻿/// <summary>
+/// </summary>
+namespace GrampsView.Common
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+    using System.Diagnostics.Contracts;
+
+    using Xamarin.CommunityToolkit.ObjectModel;
+
+    public delegate void ListedItemPropertyChangedEventHandler(IList SourceList, object Item, PropertyChangedEventArgs e);
+
+    /// <summary>
+    /// </summary>
+    public class CardGroupBase<T> : ObservableRangeCollection<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        public CardGroupBase()
+        {
+            this.CollectionChanged += Cards_CollectionChanged;
+        }
+
+        public CardGroupBase(IEnumerable<T> argList)
+        {
+            Contract.Assert(argList != null);
+
+            foreach (T item in argList)
+            {
+                base.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether [control visible].
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if [control visible]; otherwise, <c>false</c>.
+        /// </value>
+        public bool Visible
+        {
+            get
+            {
+                return !(Items is null) && (Items.Count > 0);
+            }
+        }
+
+        public new void Add(T argItem)
+        {
+            // Check if a duplicate
+            if (this.Contains(argItem))
+            {
+                return;
+            }
+
+            base.Add(argItem);
+        }
+
+        public new void Clear()
+        {
+            foreach (var item in this)
+            {
+                if (item is INotifyPropertyChanged i)
+                {
+                    i.PropertyChanged -= Element_PropertyChanged;
+                }
+            }
+
+            base.Clear();
+        }
+
+        private void Cards_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (var item in e.OldItems)
+                {
+                    if (item != null && item is INotifyPropertyChanged i)
+                    {
+                        i.PropertyChanged -= Element_PropertyChanged;
+                    }
+                }
+            }
+
+            if (e.NewItems != null)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    if (item != null && item is INotifyPropertyChanged i)
+                    {
+                        i.PropertyChanged -= Element_PropertyChanged;
+                        i.PropertyChanged += Element_PropertyChanged;
+                    }
+                }
+            }
+        }
+
+        private void Element_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ GrampsView/Common/Card Group/Grouping.cs
@@ -0,0 +1,23 @@
+﻿namespace GrampsView.Common
+{
+    using System.Collections.Generic;
+
+    using Xamarin.CommunityToolkit.ObjectModel;
+
+    public class Grouping<K, T> : ObservableRangeCollection<T>
+    {
+        public Grouping(K key, IEnumerable<T> items)
+        {
+            Key = key;
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public K Key
+        {
+            get; private set;
+        }
+    }
+}
