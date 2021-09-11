484fb9e7d5e67df0977762f8e0500178959a5b88
paul handcock <phandcock@live.com.au>
2021-09-11 10:15:52 +0800

    fix: add basic drag gesture for notefullcard

--- GrampsView/UserControls/General/NoteCardFull.xaml.cs
+++ GrampsView/UserControls/General/NoteCardFull.xaml.cs
@@ -8,5 +8,12 @@
         {
             InitializeComponent();
         }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            Label t = (sender as DragGestureRecognizer).Parent as Label;
+
+            e.Data.Text = (string)t.FormattedText;
+        }
     }
 }
