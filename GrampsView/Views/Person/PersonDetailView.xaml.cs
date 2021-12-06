daa5cbaf0f112dc48a278366944589a4147f2ba9
paul handcock <phandcock@live.com.au>
2021-12-07 01:54:59 +0800

    fix: correct viewmodel fo persondetail page

--- GrampsView/Views/Person/PersonDetailView.xaml.cs
+++ GrampsView/Views/Person/PersonDetailView.xaml.cs
@@ -1,15 +1,17 @@
 ﻿namespace GrampsView.Views
 {
+    using GrampsView.ViewModels;
+
     using Microsoft.Extensions.DependencyInjection;
 
     public partial class PersonDetailPage : ViewBase
     {
-        private PersonDetailPage _viewModel { get; set; }
+        private PersonDetailViewModel _viewModel { get; set; }
 
         public PersonDetailPage()
         {
             InitializeComponent();
-            BindingContext = _viewModel = App.Current.Services.GetService<PersonDetailPage>();
+            BindingContext = _viewModel = App.Current.Services.GetService<PersonDetailViewModel>();
         }
     }
 }
