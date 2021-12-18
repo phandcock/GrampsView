6f8c0fb6646d2779c0516757d65faee7d414ce8b
paul handcock <phandcock@live.com.au>
2021-12-18 10:51:18 +0800

    fix: non-display of event list

--- GrampsView/Views/Event/EventListView.xaml.cs
+++ GrampsView/Views/Event/EventListView.xaml.cs
@@ -1,16 +1,18 @@
 ﻿namespace GrampsView.Views
 {
+    using GrampsView.ViewModels;
+
     using Microsoft.Extensions.DependencyInjection;
 
     public sealed partial class EventListPage : ViewBase
     {
-        private EventListPage _viewModel { get; set; }
+        private EventListViewModel _viewModel { get; set; }
 
         public EventListPage()
         {
             InitializeComponent();
 
-            BindingContext = _viewModel = App.Current.Services.GetService<EventListPage>();
+            BindingContext = _viewModel = App.Current.Services.GetService<EventListViewModel>();
         }
     }
 }
