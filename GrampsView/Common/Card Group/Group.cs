49e3c257f10d89d9c84a2f769c023b833ec6dc6a
paul handcock <phandcock@live.com.au>
2022-01-12 19:22:19 +0800

    fix: nuget updates

--- GrampsView/Common/Card Group/Group.cs
+++ GrampsView/Common/Card Group/Group.cs
@@ -6,6 +6,17 @@
 
     public class Group<T> : ObservableRangeCollection<T>
     {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title
+        {
+            get; set;
+        }
+
         public Group(List<T> items)
         {
             foreach (var item in items)
