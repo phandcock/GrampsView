185097bbe8ed71824c372a17725fb252d758f999
paul handcock <phandcock@live.com.au>
2022-04-01 18:44:48 +0800

    fix: update to version 16.24

--- GrampsView.UWP/App.xaml.cs
+++ GrampsView.UWP/App.xaml.cs
@@ -25,7 +25,7 @@
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    public sealed partial class App : Windows.UI.Xaml.Application
+    public sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object. This is the first line of authored code
