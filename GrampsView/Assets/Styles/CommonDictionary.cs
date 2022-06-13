51445fb6a9f6de9f2c44e9d563bdd13907f7deb8
paul handcock <phandcock@live.com.au>
2022-06-13 20:29:53 +0800

    fix: add sharedsharpnu nuget

---
+++ GrampsView/Assets/Styles/CommonDictionary.cs
@@ -0,0 +1,12 @@
+﻿namespace GrampsView.Assets.Styles
+{
+    using Xamarin.Forms;
+
+    public partial class CommonDictionary : ResourceDictionary
+    {
+        public CommonDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
