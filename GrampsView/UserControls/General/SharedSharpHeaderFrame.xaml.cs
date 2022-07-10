a1187ca3c46f459204a9a5341f54d2cdaa77e119
paul handcock <phandcock@live.com.au>
2022-07-10 10:57:00 +0800

    fix: tweak settngs styles

---
+++ GrampsView/UserControls/General/SharedSharpHeaderFrame.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GrampsView.UserControls
+{
+    using Xamarin.Forms;
+
+    public partial class SharedSharpHeaderFrame : ControlTemplate
+    {
+        public SharedSharpHeaderFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
