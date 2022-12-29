4874fa66612cb7e4d701ded4b481a729197b4cf8
paul handcock <phandcock@live.com.au>
2022-12-29 17:29:23 +0800

    fix: force app distribution when debug

--- GrampsView/Platforms/Android/MainActivity.cs
+++ GrampsView/Platforms/Android/MainActivity.cs
@@ -1,11 +1,38 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
-namespace GrampsView
+using Microsoft.AppCenter.Distribute;
+
+using SharedSharp.Errors.Interfaces;
+
+namespace GrampsView.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+            // Enable Distribute for Debug builds
+            Distribute.SetEnabledForDebuggableBuild(true);
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            Exception newExc = new(nameof(CurrentDomainOnUnhandledException), unhandledExceptionEventArgs.ExceptionObject as Exception);
+            Ioc.Default.GetRequiredService<IErrorNotifications>().NotifyException("CurrentDomainOnUnhandledException", newExc);
+        }
+
+        private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+        {
+            Exception newExc = new(nameof(CurrentDomainOnUnhandledException), unobservedTaskExceptionEventArgs.Exception);
+            Ioc.Default.GetRequiredService<IErrorNotifications>().NotifyException("TaskSchedulerOnUnobservedTaskException", newExc);
+        }
     }
 }
